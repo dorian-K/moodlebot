165b61184cc2eafc4bea1650360b4977d480efc5
Dorian Koch <62394594+dorian-K@users.noreply.github.com>
2024-10-30 11:45:11 +0100

    Fix bug with old cookies

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import pyotp
 import os
 import atexit
 import time
+import argparse
 
 load_dotenv()
 username = os.getenv("SSO_USERNAME") # username for rwth sso
@@ -36,32 +37,50 @@ def click(driver, element, expected_text=None):
             assert expected_text in element.text, f"Expected text '{expected_text}' not found in element text '{element.text}'"
     driver.execute_script('arguments[0].click()', element)
 
-def moodle_is_logged_in(driver):
+def moodle_is_logged_in(driver: webdriver.Chrome):
     try:
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "userinitials")))
-        return True
-    except:
-        return False
+        logged_in_cond = EC.presence_of_element_located((By.CLASS_NAME, "userinitials"))
+        logged_out_cond = EC.presence_of_element_located((By.XPATH, '//*[@id="region-main"]/div/div[3]/div[2]/div/div[1]/a'))
+        WebDriverWait(driver, 5).until(EC.any_of(logged_in_cond, logged_out_cond))
 
-def perform_until_login(driver, page_url, username, password, mfa_name):
-    driver.get(page_url)
+        try:
+            if logged_in_cond(driver):
+                return True
+        except:
+            pass
+        try:
+            if logged_out_cond(driver):
+                return False
+        except:
+            pass
+        raise Exception("Unknown logged in state 1")
+    except Exception as e:
+        print("An error occurred: ", e)
+        raise Exception("Unknown logged in state 2")
+
+def perform_login(driver: webdriver.Chrome, username, password, mfa_name):
+    print("Opening moodle startpage")
+    driver.get("https://moodle.rwth-aachen.de/")
    
     if moodle_is_logged_in(driver):
         print("Session loaded from cache, login not required!")
         return
     print("Not logged in, proceeding to login...")
+    # remove all cookies
+    driver.delete_all_cookies()
     
-    click(driver, driver.find_element(By.XPATH, '//*[@id="usernavigation"]/div[3]/div/span/a'), ["Login", "Log in"])
+    # click(driver, driver.find_element(By.XPATH, '//*[@id="usernavigation"]/div[3]/div/span/a'), ["Login", "Log in"])
    
     login_btn_xpath = '//*[@id="region-main"]/div/div[3]/div[2]/div/div[1]/a'
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, login_btn_xpath)))
     click(driver, driver.find_element(By.XPATH, login_btn_xpath), "Login via RWTH Single Sign-on")
-   
+    print("Clicked 'Login via RWTH Single Sign-on'")
     WebDriverWait(driver, 10).until(EC.url_contains("sso.rwth-aachen.de"))
 
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "username"))).send_keys(username)
     driver.find_element(By.ID, "password").send_keys(password)
     click(driver, driver.find_element(By.ID, "login"), "Anmeldung")
+    print("Clicked 'Anmeldung'")
     
     tan_selector_xpath = '//*[@id="fudis_selected_token_ids_input"]'
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, tan_selector_xpath)))
@@ -71,6 +90,7 @@ def perform_until_login(driver, page_url, username, password, mfa_name):
     proceed_xpath = '//*[@id="fudiscr-form"]/button'
     proceed_elem = driver.find_element(By.XPATH, proceed_xpath)
     click(driver, proceed_elem, "Weiter")
+    print("Clicked 'Weiter'")
 
     tan_xpath = '//*[@id="fudis_otp_input"]'
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, tan_xpath)))
@@ -79,14 +99,16 @@ def perform_until_login(driver, page_url, username, password, mfa_name):
     validate_xpath = '//*[@id="fudiscr-form"]/button[1]'
     validate_element = driver.find_element(By.XPATH, validate_xpath)
     click(driver, validate_element, "Überprüfen")
+    print("Clicked 'Überprüfen'")
 
     WebDriverWait(driver, 10).until(EC.url_contains("moodle.rwth-aachen.de"))
+    print("Successfully logged in!")
     
 def take_actions(driver, page_url):
     driver.get(page_url)
-
+    print("Waiting for quiz load...")
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "modtype_quiz")))
-
+    print("Quiz loaded!")
     data_elements = driver.find_elements(By.CLASS_NAME, "modtype_quiz")
 
     prev_elems = 0
@@ -97,8 +119,8 @@ def take_actions(driver, page_url):
         pass
 
     if len(data_elements) is not prev_elems:
-        print(data_elements)
-        message = f"<@{discord_user_id}> New quiz available on moodle! Check it out at: " + page_url
+        print("Sending discord notification for", data_elements)
+        message = f"<@{discord_user_id}> New quiz available on moodle ({len(data_elements)} != {prev_elems})! Check it out at: " + page_url
         # try sending discord webhook for 3 trys
         for _ in range(3):
             try:
@@ -110,6 +132,8 @@ def take_actions(driver, page_url):
         
         with open("prev_elems.data", "w") as f:
             f.write(str(len(data_elements)))
+    else:
+        print("No new quiz available")
 
 def teardown(driver):
     # by default cookies expire after the session closes, so we update the expiry to 3 days
@@ -134,22 +158,33 @@ def lock():
         os.remove("lock")
     atexit.register(remove_lock)
 
-if __name__ == "__main__":
-    lock()
-
+def make_driver(headless=False):
     options = webdriver.ChromeOptions()
     profile_path = "./selenium_profile"  # Path to save the profile data
     options.add_argument(f"--user-data-dir={profile_path}")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    # options.add_argument('--headless')
+    if headless:
+        options.add_argument('--headless')
 
-    driver = webdriver.Chrome(options=options)
+    return webdriver.Chrome(options=options)
+
+def main(headless=False):
+    lock()
+
+    driver = make_driver(headless=headless)
     page_url = "https://moodle.rwth-aachen.de/course/view.php?id=43600"
     try:
-        perform_until_login(driver, page_url, username, password, mfa_name)
+        perform_login(driver, username, password, mfa_name)
         take_actions(driver, page_url)
     except Exception as e:
         print("An error occurred: ", e)
     finally:
         teardown(driver)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Moodle Bot")
+    parser.add_argument("--headless", action="store_true", help="Run in headless mode")
+    args = parser.parse_args()
+    main(args.headless)
+
